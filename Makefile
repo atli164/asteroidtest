683037dbb4ddd04a70e4bcd7bacc77e4b759d249
atli164 <autorsia@gmail.com>
2018-02-05 11:24:59 +0000

    Structure additions, major progress

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC := g++
 CFLAGS := -std=gnu++17 
-LIBS := `pkg-config --libs allegro-5 allegro_image-5`
+LIBS := `pkg-config --libs allegro-5 allegro_image-5 allegro_primitives-5`
 SOURCE := main.cpp
 BIN := asteroids
 
--- main.cpp
+++ main.cpp
@@ -1,54 +1,220 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
 
 const double FPS = 60;
 const double pi = acos(-1);
-const int init_sz = 200;
 
-enum ctrlkeys {
-    up, down, left, right, fire
-};
+double frand(double fmin, double fmax) {
+    double f = (double)rand() / RAND_MAX;
+    return fmin + f * (fmax - fmin);
+}
 
-bool keys[5] = {false, false, false, false, false};
-
-double x = 0, y = 0;
-double dx = 0, dy = 0;
-double theta = 0; 
-bool firing = false;
 double zoom = 1.0;
 int scr_x, scr_y;
-double scale_w, scale_h, scale_x, scale_y;
 
 ALLEGRO_DISPLAY *dsp = NULL;
-ALLEGRO_BITMAP *buf = NULL;
 ALLEGRO_EVENT_QUEUE *evq = NULL;
 ALLEGRO_TIMER *tmr = NULL;
-ALLEGRO_BITMAP *shp = NULL;
+ALLEGRO_COLOR white;
+ALLEGRO_COLOR brown;
+ALLEGRO_COLOR red;
+
+struct ship;
+struct asteroid;
+struct astfield;
+struct laser;
+
+// No x, y position since ship is by
+// default origin of coordinates
+struct ship {
+    double dx, dy, dv, ddv, phi, dphi, cooldown;
+    bool firing, hit;
+    ship() : {
+        dx = 0;
+        dy = 0;
+        dv = 0;
+        ddv = 0;
+        phi = pi / 2;
+        dphi = 0;
+        firing = false;
+        hit = false;
+        cooldown = 0;
+    }
+
+    void update() {
+        dv += ddv;
+        dv = max(dv, -1);
+        dv = min(dv, 5);
+        phi += dphi;
+        dx = cos(phi) * dv;
+        dy = sin(phi) * dv;
+        cooldown = max(0.0, cooldown - 1 / FPS);
+        if(cooldown == 0.0) {
+            astfield.add_lsr();
+            cooldown = 1;
+        }
+    }
+
+    void draw() {
+        double topx = 30 * cos(phi);
+        double topy = 30 * sin(phi);
+        double bot1x = 30 * cos(phi + pi + pi / 12);
+        double bot1y = 30 * sin(phi + pi + pi / 12);
+        double bot2x = 30 * cos(phi + pi - pi / 12);
+        double bot2y = 30 * sin(phi + pi - pi / 12);
+        al_draw_filled_triangle(topx, topy, bot1x, bot1y, bot2x, bot2y, white);
+    }
+};
+
+ship player;
+
+struct asteroid {
+    double x, y, r, dx, dy;
+    bool todel;
+    asteroid(double _x, double _y, double _r) : {
+        x = _x;
+        y = _y;
+        r = frand(25, _r);
+        dx = frand(0.5, 2);
+        dy = frand(0.5, 2);
+        todel = false;
+    }
+
+    // Returns whether it should be deleted
+    void update() {
+        x -= player.dx;
+        y -= player.dy;
+        double dist = hypot(x, y);
+        if(dist < r) {
+            player.hit = true;
+        }
+        todel = dist > 10000;
+    }
+
+    void draw() {
+        al_draw_filled_circle(x, y, r, brown);
+    }
+};
+
+struct laser {
+    double x, y, dx, dy;
+    bool todel;
+    laser() : {
+        x = 0;
+        y = 0;
+        dx = 10 * cos(player.phi);
+        dy = 10 * sin(player.phi);
+        todel = false;
+    }
+    
+    // Returns whether it should be deleted
+    bool update() {
+        x -= player.dx;
+        y -= player.dy;
+        double dist = hypot(x, y);
+        todel = dist > 10000; 
+    }
+
+    void draw() {
+        al_draw_filled_circle(x, y, 5, red);
+    }
+};
+
+struct astfield {
+    vector<asteroid> asts;
+    vector<laser> lsrs;
+    astfield() : {}
+
+    void add_ast(double _x, double _y) {
+        asts.push_back(asteroid(_x, _y, 150));
+    }
+
+    void add_lsr() {
+        lsrs.push_back(laser());
+    }
+    
+    void erase_asts() {
+        asts.erase(std::remove_if(asts.begin(), asts.end(),
+                    [](const asteroid &a) { return a.todel; }), asts.end());
+    }
+
+    void erase_lsrs() {
+        lsrs.erase(std::remove_if(lsrs.begin(), lsrs.end(),
+                    [](const laser &l) { return l.todel; }), lsrs.end());
+    }
+
+    void update() {
+        for(asteroid a : asts) {
+            a.update();
+        }
+        for(laser l : lsrs) {
+            l.update();
+        }
+        erase_asts();
+        erase_lsrs();
+        vector<asteroid> newasts;
+        for(asteroid a : asts) {
+            for(laser l : lsrs) {
+                if(hypot(l.x - a.x, l.y - a.y) < a.r) {
+                    a.todel = true;
+                    l.todel = true;
+                    if(a.r >= 50.0) {
+                        double randang = frand(0, 2 * pi);
+                        double posx1 = a.x + a.r * cos(randang) / 2;
+                        double posy1 = a.y + a.r * sin(randang) / 2;
+                        randang += pi;
+                        double posx2 = a.x + a.r * cos(randang) / 2;
+                        double posy2 = a.y + a.r * sin(randang) / 2;
+                        newasts.push_back(asteroid(posx1, posy1, a.r / 2));
+                        newasts.push_back(asteroid(posx2, posy2, a.r / 2));
+                    }
+                }
+            }
+        }
+        erase_asts();
+        erase_lsrs();
+        for(asteroid a : newasts) {
+            asts.push_back(a);
+        }
+    }
+
+    void draw() {
+        for(asteroid a : asts) {
+            a.draw();
+        }
+        for(laser l : lsrs) {
+            l.draw();
+        }
+    }
+};
+
+astfield field;
 
 void destruction() {
     al_destroy_timer(tmr);
     al_destroy_display(dsp);
-    al_destroy_bitmap(buf);
     al_destroy_event_queue(evq);
-    al_destroy_bitmap(shp);
 }
 
 void drawframe(bool &draw) {
     if(draw && al_is_event_queue_empty(evq)) {
-        al_set_target_bitmap(buf);
         al_clear_to_color(al_map_rgb(0, 0, 0));
-        al_draw_bitmap(shp, x, y, 0);
-        al_set_target_backbuffer(dsp);
-        al_clear_to_color(al_map_rgb(0, 0, 0));
-        al_draw_scaled_bitmap(buf, 0, 0, x - scr_x / 2, y - scr_y / 2, scale_x, scale_y, scale_w, scale_h, 0);
+        drawship();
+        drawasteroids();
+        drawlasers();
+        al_flip_display();
         draw = false;
     }
 }
 
 int main(int argc, char **argv) {
+    srand(time(NULL));
     
     if(!al_init()) {
         std::cerr << "Failed to initialize Allegro." << std::endl;
@@ -58,6 +224,12 @@ int main(int argc, char **argv) {
 
     if(!al_init_image_addon()) {
         std::cerr << "Failed to initialize image addon." << std::endl;
+        destruction();
+        return -1;
+    }
+
+    if(!al_init_primitive_addon()) {
+        std::cerr << "Failed to initialize primitives addon." << std::endl;
         destruction();
         return -1;
     }
@@ -75,8 +247,10 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    scr_x = al_get_display_width(dsp);
+    scr_y = al_get_display_height(dsp);
     al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
-    dsp = al_create_display(init_sz, init_sz);
+    dsp = al_create_display(scr_x, scr_y);
     if(!dsp) {
         std::cerr << "Failed to create display." << std::endl;
         destruction();
@@ -94,39 +268,18 @@ int main(int argc, char **argv) {
     al_register_event_source(evq, al_get_timer_event_source(tmr));
     al_register_event_source(evq, al_get_keyboard_event_source());
 
-    shp = al_load_bitmap("ship.png");
-    if(!shp) {
-        std::cerr << "Failed to ship load bitmap." << std::endl;
-        destruction();
-        return -1;
-    }
-
-    scr_x = al_get_display_width(dsp);
-    scr_y = al_get_display_height(dsp);
-    double scale_x = scr_x / init_sz;
-    double scale_y = scr_y / init_sz;
-    double scale_mn = std::min(scale_x, scale_y);
-    scale_w = init_sz * scale_mn;
-    scale_h = init_sz * scale_mn;
-    scale_x = (scr_x - scale_w) / 2;
-    scale_y = (scr_y - scale_h) / 2;
-
-    buf = al_create_bitmap(scr_x, scr_y);
-    if(!buf) {
-        std::cerr << "Failed to initialize buffer bitmap." << std::endl;
-        destruction();
-        return -1;
-    }
-
     al_clear_to_color(al_map_rgb(0, 0, 0));
     al_flip_display();
 
     al_start_timer(tmr);
 
-    bool run = true;
+    white = al_map_rgb(255, 255, 255);
+    brown = al_map_rgb(150, 85, 0);
+    red = al_map_rgb(255, 0, 0);
+
     bool draw = true;
 
-    while(run) {
+    while(true) {
         ALLEGRO_EVENT evt;
         ALLEGRO_TIMEOUT tmout;
 
@@ -140,43 +293,48 @@ int main(int argc, char **argv) {
                     draw = true;
                     break;
                 case ALLEGRO_EVENT_DISPLAY_CLOSE:
-                    run = false;
-                    break;
+                    destruction();
+                    return 0;
+                case ALLEGRO_EVENT_DISPLAY_RESIZE:
+                    al_acknowledge_resize(display);
+                    scr_x = al_get_display_width(dsp);
+                    scr_y = al_get_display_height(dsp);
+                    al_resize_display(dsp);
                 case ALLEGRO_EVENT_KEY_DOWN:
                     switch(evt.keyboard.keycode) {
                         case ALLEGRO_KEY_UP:
-                            keys[up] = true;
+                            player.ddv = 0.01;
                             break;
                         case ALLEGRO_KEY_DOWN:
-                            keys[down] = true;
+                            player.ddv = -0.01;
                             break;
                         case ALLEGRO_KEY_LEFT:
-                            keys[left] = true;
+                            player.dphi = 0.1;
                             break;
                         case ALLEGRO_KEY_RIGHT:
-                            keys[right] = true;
+                            player.dphi = -0.1;
                             break;
                         case ALLEGRO_KEY_SPACE:
-                            keys[fire] = true;
+                            player.firing = true;
                             break;
                     }
                     break;
                 case ALLEGRO_EVENT_KEY_UP:
                     switch(evt.keyboard.keycode) {
                         case ALLEGRO_KEY_UP:
-                            keys[up] = false;
+                            if(player.ddv > 0) player.ddv = 0;
                             break;
                         case ALLEGRO_KEY_DOWN:
-                            keys[down] = false;
+                            if(player.ddv < 0) player.ddv = 0;
                             break;
                         case ALLEGRO_KEY_LEFT:
-                            keys[left] = false;
+                            if(player.dphi > 0) player.dphi = 0;
                             break;
                         case ALLEGRO_KEY_RIGHT:
-                            keys[right] = false;
+                            if(player.dphi < 0) player.dphi = 0;
                             break;
                         case ALLEGRO_KEY_SPACE:
-                            keys[fire] = false;
+                            player.firing = false;
                             break;
                         case ALLEGRO_KEY_ESCAPE:
                             destruction();
@@ -187,12 +345,5 @@ int main(int argc, char **argv) {
         }
         drawframe(draw);
     }
-
-    al_destroy_timer(tmr);
-    al_destroy_display(dsp);
-    al_destroy_bitmap(buf);
-    al_destroy_event_queue(evq);
-    al_destroy_bitmap(shp);
-    return 0;
 }
 
